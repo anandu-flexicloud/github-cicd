66c577838305d97e1c638f33eb2f9279eb5b6e03
anandu <anandu@flexicloud.in>
2025-02-25 00:22:32 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,11 @@
+onst express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, CI/CD Pipeline! finall tewst');
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
